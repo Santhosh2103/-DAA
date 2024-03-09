f4612ac1a9f8a2495f58f0410c0a078df43c86b0
Santhosh2103 <118877983+Santhosh2103@users.noreply.github.com>
2024-03-09 08:36:15 +0530

    Update 14 minimum and maximum value.cpp

--- 14 minimum and maximum value.cpp
+++ 14 minimum and maximum value.cpp
@@ -1,44 +1,24 @@
 #include <stdio.h>
+
 int main() {
-    int n;
-    printf("Enter the number of elements in the list: ");
-    scanf("%d", &n);
-    int list[n];
-    printf("Enter the elements of the list:\n");
-    for (int i = 0; i < n; i++) {
-        printf("Element %d: ", i + 1);
-        scanf("%d", &list[i]);
-    }
-    int minSequenceStart = 0, minSequenceEnd = 0;
-    int maxSequenceStart = 0, maxSequenceEnd = 0;
-    int currentMin = list[0];
-    int currentMax = list[0];
+    int arr[] = {4, 9, 2, 7, 5, 8, 3, 1, 6};
+    int n = sizeof(arr) / sizeof(arr[0]); 
+
+    int max = arr[0];
+    int min = arr[0]; 
 
     for (int i = 1; i < n; i++) {
-        if (list[i] < currentMin) {
-            currentMin = list[i];
-            minSequenceStart = i;
-            minSequenceEnd = i;
-        } else {
-            minSequenceEnd = i;
+        if (arr[i] > max) {
+            max = arr[i]; 
         }
-        if (list[i] > currentMax) {
-            currentMax = list[i];
-            maxSequenceStart = i;
-            maxSequenceEnd = i;
-        } else {
-            maxSequenceEnd = i;
+        if (arr[i] < min) {
+            min = arr[i]; 
         }
     }
-    printf("\nMinimum value sequence: ");
-    for (int i = minSequenceStart; i <= minSequenceEnd; i++) {
-        printf("%d ", list[i]);
-    }
-    printf("\nMaximum value sequence: ");
-    for (int i = maxSequenceStart; i <= maxSequenceEnd; i++) {
-        printf("%d ", list[i]);
-    }
+
+
+    printf("Maximum value: %d\n", max);
+    printf("Minimum value: %d\n", min);
 
     return 0;
 }
-
