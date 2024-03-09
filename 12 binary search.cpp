627f1f42068e813bf246ac365b36130610d5adb4
Santhosh2103 <118877983+Santhosh2103@users.noreply.github.com>
2024-03-09 08:14:55 +0530

    Update 12 binary search.cpp

--- 12 binary search.cpp
+++ 12 binary search.cpp
@@ -1,41 +1,31 @@
 #include <stdio.h>
-#include <limits.h>
-float sum(float freq[], int i, int j) {
-    float s = 0;
-    for (int k = i; k <= j; k++)
-        s += freq[k];
-    return s;
-}
-float optimalBST(float keys[], float freq[], int n) {
-        float cost[n][n];
-    for (int i = 0; i < n; i++)
-        cost[i][i] = freq[i];
-    for (int L = 2; L <= n; L++) {
-        for (int i = 0; i <= n - L + 1; i++) {
-            int j = i + L - 1;
-            cost[i][j] = INT_MAX;
-            for (int r = i; r <= j; r++) {
-                float c = ((r > i) ? cost[i][r - 1] : 0) +
-                          ((r < j) ? cost[r + 1][j] : 0) +
-                          sum(freq, i, j);
-                cost[i][j] = (c < cost[i][j]) ? c : cost[i][j];
-            }
-        }
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        if (arr[mid] < target)
+            left = mid + 1;
+
+        else
+            right = mid - 1;
     }
-    return cost[0][n - 1];
+    return -1;
 }
+
 int main() {
-    int n;
-    printf("Enter the number of keys: ");
-    scanf("%d", &n);
-    float keys[n], freq[n];
-    printf("Enter the keys and their corresponding probabilities:\n");
-    for (int i = 0; i < n; i++) {
-        printf("Key %d: ", i + 1);
-        scanf("%f", &keys[i]);
-        printf("Frequency for Key %d: ", i + 1);
-        scanf("%f", &freq[i]);
-    }
-    printf("Cost of optimal BST is: %.2f\n", optimalBST(keys, freq, n));
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    int target = 16; 
+
+    int result = binarySearch(arr, 0, n - 1, target); 
+
+    if (result == -1)
+        printf("Element is not present in array\n");
+    else
+        printf("Element is present at index %d\n", result);
+
     return 0;
 }
